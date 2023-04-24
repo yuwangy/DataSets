56039850b7a4919a0b65cf67b72da293300075ce
Yuyang Wang <franktcsn@gmail.com>
2023-04-24 12:04:18 -0700

    add data aggregation, summarize function

--- function_cab.R
+++ function_cab.R
@@ -1,9 +1,12 @@
 # data cleaning and wrangling on cab company performance
 
+Cab_Data <- read_csv("DataSets/Cab_Data.csv")
+
 Cab_Data[order(Cab_Data$`KM Travelled`, Cab_Data$`Date of Travel`),]
 arrange(Cab_Data, Company, 'Price Charged')
 arrange(Cab_Data, 'Data of Travel')
 figure1 <- select(Cab_Data, Company,'KM Travelled', 'Price Charged')
+
 figure1
 select(figure1, Company, starts_with("Yellow Cab"))
 Cab_Data
@@ -19,3 +22,22 @@ figure2 <- ggplot(Cab_Data, aes(x = `KM Travelled`, y = `Price Charged`)) +
   ylab("Price charged to customoers")
 
 figure2
+
+Cab_Data %>%
+  group_by(Company, `Price Charged`) %>%
+  summarize(profit = sum(`Price Charged`))
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
